620597af9aefa9275c60275bd4e4a1bb0bfa2dc8
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2022-02-26 21:46:40 +0700

    hihi

--- models/connection.php
+++ models/connection.php
@@ -6,7 +6,14 @@ class DB
     {
         if (!isset(self::$instance)) 
         {
-            self::$instance = mysqli_connect("us-cdbr-east-05.cleardb.net", "b6054845c2e7dd", "ab49573f", "heroku_fcf4166d15d93c7",80);
+            self::$instance = 
+            mysqli_real_connect(
+                "us-cdbr-east-05.cleardb.net",
+                "b6054845c2e7dd",
+                "ab49573f",
+                "heroku_fcf4166d15d93c7",
+            );
+            // mysqli_connect("us-cdbr-east-05.cleardb.net", "b6054845c2e7dd", "ab49573f", "heroku_fcf4166d15d93c7",80);
             // if (mysqli_connect_errno())
             // {
             //     die("Failed to connect to MySQL: " . mysqli_connect_error());
